44f9f652b0dd65056d70e7417b77f849d4ad4340
srivalligade04 <119751137+srivalligade04@users.noreply.github.com>
2022-12-03 16:26:40 -0500

    Update LogInterceptor.java

--- src/main/java/vehicle/client/LogInterceptor.java
+++ src/main/java/vehicle/client/LogInterceptor.java
@@ -7,19 +7,17 @@ import org.slf4j.LoggerFactory;
 import java.lang.invoke.MethodHandles;
 
 public final class LogInterceptor implements ClientInterceptor {
-    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
-
     @Override
     public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> method, CallOptions callOptions, Channel next) {
         return new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(next.newCall(method, callOptions)) {
             @Override
             public void sendMessage(ReqT message) {
-                LOG.info("Sending a message");
-                LOG.info(message.toString());
+                 System.out.println("Sending a message");
+                 System.out.println(message);
 
-                LOG.info("With call options");
-                LOG.info(callOptions.toString());
-                super.sendMessage(message);
+                 System.out.println("With call options");
+                 System.out.println(callOptions);
+                 super.sendMessage(message);
             }
         };
     }
