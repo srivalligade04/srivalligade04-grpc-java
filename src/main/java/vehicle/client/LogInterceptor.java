fa6bae0ed85df39e5bba632ef0ed01f508ad8504
srivalligade04 <119751137+srivalligade04@users.noreply.github.com>
2022-12-03 16:26:55 -0500

    Update LogInterceptor.java

--- src/main/java/vehicle/client/LogInterceptor.java
+++ src/main/java/vehicle/client/LogInterceptor.java
@@ -1,9 +1,6 @@
 package vehicle.client;
 
 import io.grpc.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.lang.invoke.MethodHandles;
 
 public final class LogInterceptor implements ClientInterceptor {
