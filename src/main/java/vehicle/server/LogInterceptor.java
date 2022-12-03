aeaa1012781543291c443ca9d410ba7464a04e26
srivalligade04 <119751137+srivalligade04@users.noreply.github.com>
2022-12-03 16:43:14 -0500

    Update LogInterceptor.java

--- src/main/java/vehicle/server/LogInterceptor.java
+++ src/main/java/vehicle/server/LogInterceptor.java
@@ -1,24 +1,20 @@
 package vehicle.server;
 
 import io.grpc.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
 
 public final class LogInterceptor implements ServerInterceptor {
-
-    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
 
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
         return new ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT>(next.startCall(call, headers)) {
             @Override
             public void onMessage(ReqT message) {
-                LOG.info("Receive a message");
-                LOG.info(message.toString());
-                LOG.info("With headers");
-                LOG.info(headers.toString());
+                System.out.println("Receive a message");
+                System.out.println(message.toString());
+                System.out.println("With headers");
+                System.out.println(headers.toString());
                 super.onMessage(message);
             }
         };
